f060071da4bc4b5fc7d750d7fdc57c787f176453
Viktor Polak <viktor.polak@canterbury.ac.nz>
2020-06-05 19:34:04 +1200

    progress

--- server/data/prepare2.sh
+++ server/data/prepare2.sh
@@ -6,3 +6,4 @@ TIPPECANOE="/opt/tippecanoe/bin/tippecanoe"
 
 $TIPPECANOE -l vspr -n "Measured Vs30 Sites" -Z0 -z14 -rg -o vspr.mbtiles vspr.geojson --force
 $TIPPECANOE -l aak_map -n "AhdiAK Geology Category Map" -Z0 -z14 -rg -o aak_map.mbtiles aak_map.geojson --force
+$TIPPECANOE -l ip_map -n "Iwahashi and Pike Terrain Category Map" -Z0 -z14 -rg -o ip_map.mbtiles iwahashipike.geojson --force
