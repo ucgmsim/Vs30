bc7ef1ccbc5467340d5ed7c15b6097331a8fdc10
Viktor Polak <viktor.polak@canterbury.ac.nz>
2020-07-01 15:03:33 +1200

    slip into cheaper pricing band

--- server/data/prepare2.sh
+++ server/data/prepare2.sh
@@ -4,9 +4,9 @@
 #
 TIPPECANOE="/opt/tippecanoe/bin/tippecanoe"
 
-$TIPPECANOE -l vspr -n "Measured Vs30 Sites" -Z0 -z14 -rg -o vspr.mbtiles vspr.geojson --force
-$TIPPECANOE -l aak_map -n "AhdiAK Geology Category Map" -Z0 -z14 -rg -o aak_map.mbtiles aak_map.geojson --force
-$TIPPECANOE -l ip_map -n "Iwahashi and Pike Terrain Category Map" -Z0 -z14 -rg -o ip_map.mbtiles iwahashipike.geojson --force --coalesce-fraction-as-needed
-$TIPPECANOE -l aak_vs30 -n "AhdiAK Vs30 Map" -Z0 -z14 -rg -o aak.mbtiles aak.geojson --force --coalesce-fraction-as-needed
-$TIPPECANOE -l yca_vs30 -n "YongCA Vs30 Map" -Z0 -z14 -rg -o yca.mbtiles yca.geojson --force --coalesce-fraction-as-needed
-$TIPPECANOE -l combined_vs30 -n "Combined Vs30 Map" -Z0 -z14 -rg -o combined.mbtiles combined.geojson --force --coalesce-fraction-as-needed
+$TIPPECANOE -l vspr -n "Measured Vs30 Sites" -Z0 -z13 -rg -o vspr.mbtiles vspr.geojson --force
+$TIPPECANOE -l aak_map -n "AhdiAK Geology Category Map" -Z0 -z13 -rg -o aak_map.mbtiles aak_map.geojson --force
+$TIPPECANOE -l ip_map -n "Iwahashi and Pike Terrain Category Map" -Z0 -z13 -rg -o ip_map.mbtiles iwahashipike.geojson --force --coalesce-fraction-as-needed
+$TIPPECANOE -l aak_vs30 -n "AhdiAK Vs30 Map" -Z0 -z13 -rg -o aak.mbtiles aak.geojson --force --coalesce-fraction-as-needed
+$TIPPECANOE -l yca_vs30 -n "YongCA Vs30 Map" -Z0 -z13 -rg -o yca.mbtiles yca.geojson --force --coalesce-fraction-as-needed
+$TIPPECANOE -l combined_vs30 -n "Combined Vs30 Map" -Z0 -z13 -rg -o combined.mbtiles combined.geojson --force --coalesce-fraction-as-needed
