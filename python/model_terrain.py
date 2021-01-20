bb65cc9ce1b033dcfac6500aa1eb26cbf980f2d1
Viktor Polak <viktor.polak@canterbury.ac.nz>
2021-01-21 11:31:08 +1300

    python terrain fixups, tested

--- python/model_terrain.py
+++ python/model_terrain.py
@@ -103,15 +103,18 @@ def gidx2val(model, gidx):
 
 def save(vals, x0, y0, nx, ny, xd, yd, filename):
     driver = gdal.GetDriverByName("GTiff")
-    gfile = driver.Create(filename, xsize=nx, ysize=ny, bands=1, eType=gdal.GDT_Float32)
+    # https://gdal.org/drivers/raster/gtiff.html
+    # TILED=YES much smaller (entire nan blocks), slower with eg: QGIS
+    # COMPRESS=DEFLATE smaller, =LZW larger
+    gfile = driver.Create(filename, xsize=nx, ysize=ny, bands=1,
+                          eType=gdal.GDT_Float32, options=["COMPRESS=DEFLATE"])
     gfile.SetGeoTransform([x0, xd, 0, y0, 0, yd])
     # projection
-    #srs = osr.SpatialReference()
-    #srs.ImportFromEPSG(2193)
-    #gfile.SetProjection(srs.ExportToWkt())
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(2193)
+    gfile.SetProjection(srs.ExportToWkt())
     # data
-    raster = vals.reshape(ny, nx)
-    gfile.GetRasterBand(1).WriteArray(raster)
+    gfile.GetRasterBand(1).WriteArray(vals.reshape(ny, nx))
     # close file
     gfile = None
     
