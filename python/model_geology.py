62d8cd64c08e3baa2b5fc59181d6b3ad29450caa
Viktor Polak <viktor.polak@canterbury.ac.nz>
2021-01-22 16:53:07 +1300

    found solution rasterization

--- python/model_geology.py
+++ python/model_geology.py
@@ -2,7 +2,7 @@
 import os
 
 import numpy as np
-from osgeo import gdal, ogr
+from osgeo import gdal, ogr, osr
 
 PREFIX = "/run/media/vap30/Hathor/work/plotting_data/Vs30/"
 QMAP = os.path.join(PREFIX, "qmap/qmap.shp")
@@ -18,25 +18,47 @@ def gidx(points):
     lay = shp.GetLayer(0)
     col = lay.GetLayerDefn().GetFieldIndex("gid")
 
-    #geo_ref = lyr_in.GetSpatialRef()
-    #point_ref=ogr.osr.SpatialReference()
-    #point_ref.ImportFromEPSG(4326)
-    #ctran=ogr.osr.CoordinateTransformation(point_ref,geo_ref)
-    #[lon,lat,z]=ctran.TransformPoint(lon,lat)
+    values = np.empty(len(points), dtype=np.float32)
+    values[...] = np.nan
     pt = ogr.Geometry(ogr.wkbPoint)
-    pt.SetPoint_2D(0, 1561153, 5171247)
-    pt.SetPoint_2D(0, 1569329, 5164429)
-    for p in points:
-        pt.SetPoint_2D(0, int(p[0]), int(p[1]))
+    for i, p in enumerate(points):
+        # why not decimal values??
+        pt.AddPoint_2D(round(p[0]), round(p[1]))
         lay.SetSpatialFilter(pt)
-        for feat in lay:
-            print(feat.GetField(col))
-
-    points = np.array([[1561153, 5171247], [1568536, 5169878]])
-    pts = ogr.Geometry(ogr.wkbMultiPoint)
-
+        f = lay.GetNextFeature()
+        if f is not None:
+            values[i] = f.GetField(col)
 
     return values
+    
+
+def gidx_grid(xmin, xmax, ymin, ymax, xd, yd):
+    """
+    Optimised polygon interpolation using geotiff rasterisation.
+    """
+    xmin = 1000000
+    xmax = 2126400
+    ymin = 4700000
+    ymax = 6338400
+    xd = 100
+    yd = 100
+    width = round((xmax - xmin) / xd)
+    height = round((ymax - ymin) / yd)
+    shp = ogr.Open(QMAP, gdal.GA_ReadOnly)
+    lay = shp.GetLayer(0)
+    
+    srs = osr.SpatialReference()
+    srs.ImportFromEPSG(2193)
+    ds = gdal.Rasterize("new.tif",
+                        QMAP,
+                        outputSRS=srs,
+                        outputBounds=[xmin, ymin, xmax, ymax],
+                        xRes=xd,
+                        yRes=yd,
+                        #noData=255,
+                        targetAlignedPixels=True,
+                        attribute="gid",
+                        outputType=gdal.GDT_Float32)
     
 
 # grid setup
@@ -49,28 +71,11 @@ yd = 100
 nx = round((xn - x0) / xd + 1)
 ny = round((yn - y0) / yd + 1)
 
+
+
+exit()
 # run
 points = np.vstack(np.mgrid[x0:xn + 1:xd,y0:yn + 1:yd].T)
-
-points = points[:20000000]
-pts = ogr.Geometry(ogr.wkbMultiPoint)
-pt = ogr.Geometry(ogr.wkbPoint)
-arr=np.empty(20000000)
-arr[...] = np.nan
-t1 = time()
-for i, p in enumerate(points):
-    pt.AddPoint_2D(int(p[0]), int(p[1]))
-    #_=pts.AddGeometry(pt)
-    lay.SetSpatialFilter(pt)
-    f = lay.GetNextFeature()
-    if f is not None:
-        arr[i] = f.GetField(col)
-t2=time()
-
-# 13min / 20million points
-# total 184 million = 117 minutes, 2 core hours
-
-
 gids = gidx(points)
 model = model_posterior_paper()
 vals = gidx2val(model, gids)
