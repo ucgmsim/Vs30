e1de4ff5dd54a6637a6f2ff1fb12c35ac580ff2c
Viktor Polak <viktor.polak@canterbury.ac.nz>
2021-02-12 17:09:21 +1300

    mvn.py integration

--- vs30/mvn.py
+++ vs30/mvn.py
@@ -2,77 +2,106 @@ import math
 
 import numpy as np
 
-obs_locations = np.array([[1997000, 5502000], [2000000, 5500000], [1998000, 5501000]])
-model_locations = np.array(
-    [[1999000, 5500000], [2000000, 5500000], [2002000, 5500000], [2220000, 5500000]]
-)
-model_variances = np.array([0.2, 0.1, 0.5, 0.5]) ** 2
-modeledValues = np.log(np.array([400, 500, 600, 800]))
+obs_locs = np.array([[1997000, 5502000], [2000000, 5500000], [1998000, 5501000]])
+obs_vs30 = np.array([50, 1500, 1500])
+obs_stdv = np.array([0.1, 0.1, 0.1])
+
 modelVarObs = np.array([0.2, 0.1, 0.2]) ** 2
 logModVs30obs = np.log(np.array([300, 500, 800]))
-obs_residuals = np.log(np.array([50, 1500, 1500])) - logModVs30obs
-obs_stdev_log = np.array([0.1, 0.1, 0.1])
 
-cov_reduc = 1.5
-use_noisy_measurements = True
-phi = 993  # terrain
-phi = 1407  # geology
-min_dist = 0.1 # 10 cm
-max_dist = 10034.4365 # 10 km, keep results same by using one of the interp values 128points/2000e3 max_dist
-dist_points = 88
+model_locs = np.array(
+    [[1999000, 5500000], [2000000, 5500000], [2002000, 5500000], [2220000, 5500000]]
+)
+model_stdv = np.array([0.2, 0.1, 0.5, 0.5])
+model_vs30 = np.log(np.array([400, 500, 600, 800]))
 
 
-def mvn(sites, phi, cov_reduc=1.5, use_noisy_measurements=True, min_dist=0.1, max_dist=10034.4365, dist_points=88):
-    dist = np.exp(np.linspace(math.log(min_dist), math.log(max_dist), dist_points))
-    corr = 1 / np.e ** (dist / phi)
-    
+def corr_func(distances, phi):
+    """
+    Correlation function by distance.
+    phi is 993 for terrain model, 1407 for geology
+    """
+    # original was interpolated from distances:
+    # exp(np.linspace(log(0.1), log(2000e3), 128))
+    # minimum distance of 0.1 metres enforced
+    return 1 / np.e ** (np.maximum(0.1, distances) / phi)
+
+
+def tcrossprod(x):
+    """
+    Cross product from a 1d numpy array.
+    """
+    return x[:, np.newaxis] * x
+
+
+def dists(x):
+    """
+    Euclidean distance from 2d diff array.
+    """
+    #distances = np.linalg.norm(x, axis=1)
+    # alternative, may be faster
+    distances = np.sqrt(np.einsum("ij,ij->i", x, x))
+
+
+def xy2complex(x):
+    """
+    Convert array of 2D coordinates to array of 1D complex numbers.
+    """
+    c = x[:, 0].astype(np.complex64)
+    c.imag += x[:, 1]
+    return c
+
+
+def dist_mat(x):
+    """
+    Distance matrix between coordinates (complex numbers) or simple values.
+    """
+    return np.abs(x[:, np.newaxis] - x)
+
+
+def mvn(model_locs, model_vs30, model_stdv, obs_locs, obs_vs30, obs_stdv, sites, phi, cov_reduc=1.5, noisy=True, min_dist=0.1, max_dist=10000, dist_points=88):
+    """
+    Modify model with observed locations.
+    phi: correlation range factor, see corr_func()
+    noisy: noisy measurements
+    """
+    obs_residuals = np.log(obs_vs30) - logModVs30obs
+
     # Wea equation 33, 40, 41
-    if use_noisy_measurements:
-        omegaObs = np.sqrt(modelVarObs / (modelVarObs + obs_stdev_log ** 2))
-        # don't use *= to prevent overwriting function input
-        obs_residuals = omegaObs * obs_residuals
+    if noisy:
+        omega_obs = np.sqrt(modelVarObs / (modelVarObs + obs_stdv ** 2))
+        obs_residuals *= omega_obs
     
     # default outputs if no sites closeby
-    pred = np.copy(modeledValues)
-    var = model_variances * corr[0]
-    
-    for i in range(len(model_locations)):
-        # model point to observations
-        # distances = np.linalg.norm(obs_locations - model_locations[i], axis=1)
-        dist_diffs = (obs_locations - model_locations[i]).T
-        distances = np.sqrt(np.einsum("ij,ij->j", dist_diffs, dist_diffs))
+    pred = np.log(model_vs30)
+    var = model_stdv * model_stdv * corr_func(0, phi)
+
+    # model point to observations
+    for i in range(len(model_locs)):
+        distances = dists(obs_locs - model_locs[i])
         wanted = distances < max_dist
         if max(wanted) == False:
-            # not close enough to any points
+            # not close enough to any observed locations
             continue
     
-        # distances between all of interest
-        locs = np.vstack((model_locations[i], obs_locations[wanted]))
-        locs = locs[:, 0] + 1j * locs[:, 1]
-        cov_matrix = abs(locs[:, np.newaxis] - locs)
-        # correlation function - use interp to keep same behaviour at edges
-        cov_matrix = np.interp(cov_matrix, dist, corr)
+        # distances between interesting points
+        cov_matrix = dist_mat(xy2complex(np.vstack((model_locs[i], obs_locs[wanted]))))
+        # correlation
+        cov_matrix = corr_func(cov_matrix, phi)
         # uncertainties
-        uncer = np.sqrt(np.insert(modelVarObs[wanted], 0, model_variances[i]))
-        cov_matrix = cov_matrix * uncer[:, np.newaxis] * uncer
+        cov_matrix *= tcrossprod(np.insert(np.sqrt(modelVarObs[wanted]), 0, model_stdv[i]))
     
-        if use_noisy_measurements:
-            omega = np.insert(omegaObs[wanted], 0, 1)
-            omega = omega[:, np.newaxis] * omega
-            omega[np.eye(len(omega), dtype=np.bool)] = 1
-            cov_matrix *= omega
-    
+        if noisy:
+            cov_matrix *= np.fill_diagonal(tcrossprod(np.insert(omega_obs[wanted], 0, 1)), 1)
+
         # covariance reduction factors
         if cov_reduc > 0:
-            model = np.insert(logModVs30obs[wanted], 0, modeledValues[i])
-            cov_matrix *= np.exp(-cov_reduc * np.abs(model[:, np.newaxis] - model))
+            cov_matrix *= np.exp(-cov_reduc * dist_mat(np.insert(logModVs30obs[wanted], 0, pred[i])))
     
         inv_matrix = np.linalg.inv(cov_matrix[1:, 1:])
-        pred[i] = modeledValues[i] + np.dot(
-            np.dot(cov_matrix[0, 1:], inv_matrix), obs_residuals[wanted]
-        )
+        pred[i] += np.dot(np.dot(cov_matrix[0, 1:], inv_matrix), obs_residuals[wanted])
         var[i] = cov_matrix[0, 0] - np.dot(
             np.dot(cov_matrix[0, 1:], inv_matrix), cov_matrix[1:, 0]
         )
     
-    return(pred, var)
+    return pred, var
