076a058e0db263d3ba6d0c90ea52ec70e989211c
Viktor Polak <viktor.polak@canterbury.ac.nz>
2021-03-04 15:02:31 +1300

    fixed up qgis project files

--- vs30calc.py
+++ vs30calc.py
@@ -7,7 +7,7 @@ from functools import partial
 import math
 from multiprocessing import Pool
 import os
-from shutil import rmtree
+from shutil import copyfile, rmtree
 import sys
 from time import time
 
@@ -165,9 +165,18 @@ if p_geol.update != "off" and p_terr.update != "off":
         model.combine_tiff(p_paths.out, "combined_mvn.tif", p_grid, p_comb, *tiffs_mvn)
     print(f"{time()-t:.2f}s")
 
-# save point based data
+# save point based data and copy qgis project files
 sites.to_csv(os.path.join(p_paths.out, "measured_sites.csv"), na_rep="NA", index=False)
 if p_ll is not None:
     table.to_csv(os.path.join(p_paths.out, "vs30points.csv"), na_rep="NA", index=False)
+    copyfile(
+        os.path.join(sites_load.data, "qgis_points.qgz"),
+        os.path.join(p_paths.out, "qgis.qgz"),
+    )
+else:
+    copyfile(
+        os.path.join(sites_load.data, "qgis_rasters.qgz"),
+        os.path.join(p_paths.out, "qgis.qgz"),
+    )
 
 print("complete.")
