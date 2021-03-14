b380949d4e01940f04b18f0c46c1d3c79e674827
Viktor Polak <viktor.polak@canterbury.ac.nz>
2021-03-15 11:27:43 +1300

    variable name

--- vs30/mvn.py
+++ vs30/mvn.py
@@ -121,18 +121,17 @@ def _mvn(
         if min_dist > max_dist:
             # not close enough to any observed locations
             continue
-        # boolean array mask for observed locations
-        wanted = distances <= min(max_dist, cutoff_dist)
+        loc_mask = distances <= min(max_dist, cutoff_dist)
 
         # distances between interesting points
-        cov_matrix = _dist_mat(_xy2complex(np.vstack((model_loc, obs_locs[wanted]))))
+        cov_matrix = _dist_mat(_xy2complex(np.vstack((model_loc, obs_locs[loc_mask]))))
         # correlation
         cov_matrix = _corr_func(cov_matrix, model_name)
         # uncertainties
-        cov_matrix *= _tcrossprod(np.insert(obs_model_stdv[wanted], 0, model_stdv[i]))
+        cov_matrix *= _tcrossprod(np.insert(obs_model_stdv[loc_mask], 0, model_stdv[i]))
 
         if noisy:
-            omega = _tcrossprod(np.insert(omega_obs[wanted], 0, 1))
+            omega = _tcrossprod(np.insert(omega_obs[loc_mask], 0, 1))
             np.fill_diagonal(omega, 1)
             cov_matrix *= omega
 
@@ -142,7 +141,7 @@ def _mvn(
                 -cov_reduc
                 * _dist_mat(
                     np.insert(
-                        np.log(sites.loc[wanted, f"{model_name}_vs30"].values),
+                        np.log(sites.loc[loc_mask, f"{model_name}_vs30"].values),
                         0,
                         pred[i],
                     )
@@ -150,7 +149,9 @@ def _mvn(
             )
 
         inv_matrix = np.linalg.inv(cov_matrix[1:, 1:])
-        pred[i] += np.dot(np.dot(cov_matrix[0, 1:], inv_matrix), obs_residuals[wanted])
+        pred[i] += np.dot(
+            np.dot(cov_matrix[0, 1:], inv_matrix), obs_residuals[loc_mask]
+        )
         var[i] = cov_matrix[0, 0] - np.dot(
             np.dot(cov_matrix[0, 1:], inv_matrix), cov_matrix[1:, 0]
         )
