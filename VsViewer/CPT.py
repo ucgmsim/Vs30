88b6d12086c3e1fe83e9343ae921c8904e85e03e
joelridden <jriddenw@gmail.com>
2022-06-23 11:36:41 +1200

    Read and plot CPT files

---
+++ VsViewer/CPT.py
@@ -0,0 +1,71 @@
+import numpy as np
+from pathlib import Path
+
+
+class CPT:
+    """
+    Contains the data from a CPT file
+    """
+
+    def __init__(self, cpt_ffp: str):
+        self.cpt_ffp = Path(cpt_ffp)
+        depth, qc, fs, u, info = self.get_cpt_data()
+        self.depth = depth
+        self.Qc = qc
+        self.Fs = fs
+        self.u = u
+        self.info = info
+
+    def get_cpt_data(self):
+        """Get CPT data and return [z; qc; fs; u2; info]"""
+        data = np.loadtxt(self.cpt_ffp, dtype=float, delimiter=",", skiprows=1)
+
+        # Get CPT info
+        info = dict()
+        info["z_min"] = np.round(data[0, 0], 2)
+        info["z_max"] = np.round(data[-1, 0], 2)
+        info["z_spread"] = np.round(data[-1, 0] - data[0, 0], 2)
+
+        # Filtering
+        data = data[(np.all(data[:, [0]] < 30, axis=1)).T]  # z is less then 30 m
+        info["Removed rows containing 0 or below Fs or Qc values"] = not np.alltrue(
+            data[:, [1, 2]] > 0
+        )
+        data = data[np.all(data[:, [1, 2]] > 0, axis=1)]  # delete rows with zero qc, fs
+        z_raw = data[:, 0]  # m
+        qc_raw = data[:, 1]  # MPa
+        fs_raw = data[:, 2]  # MPa
+        u_raw = data[:, 3]  # Mpa
+
+        if len(data) == 0:
+            raise Exception("CPT File has no valid lines")
+
+        downsize = np.arange(z_raw[0], 30, 0.02)
+        z = np.array([])
+        qc = np.array([])
+        fs = np.array([])
+        u = np.array([])
+        for j in range(len(downsize)):
+            for i in range(len(z_raw)):
+                if abs(z_raw[i] - downsize[j]) < 0.001:
+                    z = np.append(z, z_raw[i])
+                    qc = np.append(qc, qc_raw[i])
+                    fs = np.append(fs, fs_raw[i])
+                    u = np.append(u, u_raw[i])
+
+        if len(u) > 50:
+            while u[50] >= 10:
+                u = u / 1000  # account for differing units
+
+        # some units are off - so need to see if conversion is needed
+        if len(fs) > 100:
+            # Account for differing units
+            if fs[100] > 1.0:
+                fs = fs / 1000
+        elif len(fs) > 5:
+            if fs[5] > 1.0:
+                fs = fs / 1000
+        else:
+            fs = fs
+
+        return z, qc, fs, u, info
---
+++ VsViewer/plot_cpt.py
@@ -0,0 +1,85 @@
+import argparse
+import colorsys
+from typing import List
+
+from matplotlib import colors, pyplot
+
+from CPT import CPT
+
+
+def scale_saturation(color: str, scale: float):
+    """
+    Scales the saturation down of the color by a given percentage
+    """
+    rgb = colors.colorConverter.to_rgb(color)
+    hls = colorsys.rgb_to_hls(*rgb)
+    hls_scaled = (hls[0], hls[1], hls[2] * ((100 - scale) / 100))
+    return colorsys.hls_to_rgb(*hls_scaled)
+
+
+def plot_cpt(cpts: List[CPT], output_ffp: str):
+    """
+    Plots the CPT values Qc, Fs and u at their depth values
+    and saves to a given output file
+    """
+    fig = pyplot.figure(figsize=(16, 10))
+    measurements = ["Qc", "Fs", "u"]
+    colours = ["Blue", "Green", "Red"]
+    plot_legend = len(cpts) != 1
+    scales = [i * (100 / len(cpts)) for i in range(len(cpts))]
+
+    for ix, measure in enumerate(measurements):
+        ax1 = fig.add_subplot(1, 3, ix + 1)
+        ax1.set_xlabel(f"{measure} (MPa)", size=16)
+        ax1.set_ylabel("Depth (m)", size=16)
+        for cpt_ix, cpt in enumerate(cpts):
+            pyplot.plot(
+                getattr(cpt, measure),
+                cpt.depth,
+                color=scale_saturation(colours[ix], scales[cpt_ix]),
+                linewidth=2.5,
+                label=cpt.cpt_ffp.stem,
+            )
+        if plot_legend:
+            ax1.legend(loc="upper right")
+        pyplot.gca().invert_yaxis()
+        pyplot.xticks(fontsize=13)
+        pyplot.yticks(fontsize=13)
+
+    pyplot.subplots_adjust(
+        left=0.1, bottom=0.075, right=0.9, top=0.925, wspace=0.3, hspace=0.3
+    )
+    pyplot.savefig(f"{output_ffp}.png")
+
+
+def main():
+    """
+    Gather metadata from each realisation and outputs to a csv
+    """
+    # Get args
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "cpt_ffps", type=str, nargs="+", help="The full file path to the cpt files"
+    )
+    parser.add_argument(
+        "output_ffp",
+        type=str,
+        help="The full file path to the output file (without extension)",
+    )
+    args = parser.parse_args()
+
+    # Get CPT
+    cpts = [CPT(cpt) for cpt in args.cpt_ffps]
+
+    # Plot cpt
+    plot_cpt(cpts, args.output_ffp)
+
+    # Print CPT info
+    for cpt in cpts:
+        print(f"{cpt.cpt_ffp.stem} Info")
+        for k, v in cpt.info.items():
+            print(f"{k}: {v}")
+
+
+if __name__ == "__main__":
+    main()
