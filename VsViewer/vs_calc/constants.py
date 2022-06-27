3e2eff54cacb26150a17bc8101c830acb2c19a98
joelridden <jriddenw@gmail.com>
2022-06-28 10:48:59 +1200

    Correct merge

--- VsViewer/vs_calc/constants.py
+++ VsViewer/vs_calc/constants.py
@@ -1,9 +1,9 @@
-from .get_vs_correlations import andrus, robertson, hegazy, mcgann, mcgann2
+from .get_vs_correlations import andrus_2007, robertson_2009, hegazy_2006, mcgann_2015, mcgann_2018
 
 CORRELATIONS = {
-    "andrus": andrus,
-    "robertson": robertson,
-    "hegazy": hegazy,
-    "mcgann": mcgann,
-    "mcgann2": mcgann2,
+    "andrus_2007": andrus_2007,
+    "robertson_2009": robertson_2009,
+    "hegazy_2006": hegazy_2006,
+    "mcgann_2015": mcgann_2015,
+    "mcgann_2018": mcgann_2018,
 }
