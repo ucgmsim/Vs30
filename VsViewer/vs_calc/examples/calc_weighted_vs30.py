84eb2316c038165e053682e13121374e4738a4e9
joelridden <jriddenw@gmail.com>
2022-06-28 15:12:23 +1200

    Changing example imports

--- VsViewer/vs_calc/examples/calc_weighted_vs30.py
+++ VsViewer/vs_calc/examples/calc_weighted_vs30.py
@@ -3,20 +3,20 @@ Compute the average weighted Vs30 given CPT files and correlations example
 """
 from pathlib import Path
 
-from VsViewer import vs_calc
+from VsViewer.vs_calc import CPT, VsProfile, calculate_weighted_vs30
 
 
 examples_dir = Path(__file__).parent.resolve()
 cpt_ffps = [examples_dir / "CPT_6457.csv", examples_dir / "CPT_6458.csv"]
-cpts = [vs_calc.CPT(str(cpt_ffp)) for cpt_ffp in cpt_ffps]
+cpts = [CPT(str(cpt_ffp)) for cpt_ffp in cpt_ffps]
 correlations = ["andrus_2007", "mcgann_2018"]
 correlation_weights = {"andrus_2007": 0.6, "mcgann_2018": 0.4}
 cpt_weights = {"CPT_6457": 0.8, "CPT_6458": 0.2}
 
 vs_profiles = [
-    vs_calc.VsProfile(cpt, correlation) for cpt in cpts for correlation in correlations
+    VsProfile(cpt, correlation) for cpt in cpts for correlation in correlations
 ]
-vs30, vs30_sd = vs_calc.calculate_weighted_vs30(
+vs30, vs30_sd = calculate_weighted_vs30(
     vs_profiles, cpt_weights, correlation_weights
 )
 print(f"The weighted average Vs30 is {vs30} and the Standard Deviation is {vs30_sd}")
