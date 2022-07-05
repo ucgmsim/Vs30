e70bf6a5905ae9cc270f12d7351018288530d447
joelridden <jriddenw@gmail.com>
2022-07-06 10:20:21 +1200

    Adding more pages

---
+++ VsViewer/frontend/src/components/VsProfile.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VsProfile = () => {
+  return (
+    <div className="container-fluid max-width">
+        VsProfile
+    </div>
+  );
+};
+
+export default VsProfile;
