77f6d7f82913e9f30f1fdcb502d4f1e8af70a89e
joelridden <jriddenw@gmail.com>
2022-07-19 11:06:19 +1200

    Personal Review

--- VsViewer/frontend/src/Constants.js
+++ VsViewer/frontend/src/Constants.js
@@ -9,18 +9,19 @@ export const GET_CORRELATIONS_ENDPOINT = "/api/cpt/correlations";
 export const CPT_MIDPOINT_ENDPOINT = "/api/plot/cpt/midpoint";
 
 // VsProfile Endpoints
-export const VSPROFILE_FROM_CPT_ENDPOINT = "/api/vsprofile/cpt/create"
+export const VSPROFILE_FROM_CPT_ENDPOINT = "/api/vsprofile/cpt/create";
 export const VS_PROFILE_MIDPOINT_ENDPOINT = "/api/plot/vsprofile/midpoint";
 
 // Plots
-export const DEFAULTCOLOURS = ['#1f77b4',
-'#ff7f0e',
-'#2ca02c',  
-'#d62728',    
-'#9467bd',       
-'#8c564b',         
-'#e377c2',
-'#7f7f7f',      
-'#bcbd22',
-'#17becf'
+export const DEFAULTCOLOURS = [
+  "#1f77b4", // Muted Blue
+  "#ff7f0e", // Safety Orange
+  "#2ca02c", // Cooked Asparagus Green
+  "#d62728", // Brick Red
+  "#9467bd", // Muted Purple
+  "#8c564b", // Chestnut Brown
+  "#e377c2", // Raspberyy Yogurt Pink
+  "#7f7f7f", // Middle Gray
+  "#bcbd22", // Curry Yellow-Green
+  "#17becf", // Blue-Teal
 ];
