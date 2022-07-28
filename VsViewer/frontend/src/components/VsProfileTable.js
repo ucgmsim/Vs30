08d803aa5175887d9e65e736f8f6082e9d7285f3
joelridden <jriddenw@gmail.com>
2022-07-28 12:40:34 +1200

    Tables fixes

--- VsViewer/frontend/src/components/VsProfileTable.js
+++ VsViewer/frontend/src/components/VsProfileTable.js
@@ -2,28 +2,29 @@ import React, { memo } from "react";
 
 import * as Utils from "Utils";
 
-import "assets/sptTable.css";
+import "assets/vsProfileTable.css";
 
 const VsProfileTable = ({ vsProfileData, vsProfileInfo }) => {
   if (vsProfileData !== undefined && vsProfileData !== null) {
     const vsProfileRows = [];
     vsProfileData.forEach((row, rowIdx) => {
       vsProfileRows.push(
-        <tr key={rowIdx}>
-          <td className="col-size">{Utils.roundValue(row["Depth"][rowIdx])}</td>
-          <td className="col-size">
-            {Utils.roundValue(row["Vs"][rowIdx])}
-          </td>
-          <td className="col-size">
-            {Utils.roundValue(row["Vs_SD"][rowIdx])}
-          </td>
+        <tr
+          className={
+            vsProfileInfo["removed_rows"].includes(rowIdx) ? "highlight" : ""
+          }
+          key={rowIdx}
+        >
+          <td className="col-size">{Utils.roundValue(row["Depth"])}</td>
+          <td className="col-size">{Utils.roundValue(row["Vs"])}</td>
+          <td className="col-size">{Utils.roundValue(row["Vs_SD"])}</td>
         </tr>
       );
     });
 
     return (
       <div className="center-elm">
-        <table className="spt-raw table thead-dark table-striped table-bordered mt-2 w-auto">
+        <table className="vs-raw table thead-dark table-striped table-bordered mt-2 w-auto">
           <thead>
             <tr>
               <th className="col-size" scope="col">
@@ -37,11 +38,11 @@ const VsProfileTable = ({ vsProfileData, vsProfileInfo }) => {
               </th>
             </tr>
           </thead>
-          <tbody className="tbl-width spt-scroll-tbl">
+          <tbody className="tbl-width vs-scroll-tbl">
             <tr>
               <td className="tbl-width" colSpan="4">
-                <div className="spt-scroll-tbl">
-                  <table className="spt-tbl-width">
+                <div className="vs-scroll-tbl">
+                  <table className="vs-tbl-width">
                     <tbody>{vsProfileRows}</tbody>
                   </table>
                 </div>
@@ -49,31 +50,31 @@ const VsProfileTable = ({ vsProfileData, vsProfileInfo }) => {
             </tr>
           </tbody>
         </table>
-        <div className="row two-column-row center-elm spt-info">
-          <div className="spt-min-max col-3 center-elm">
-            <table className="spt-min-max table thead-dark table-striped table-bordered mt-2 w-auto">
+        <div className="row two-column-row center-elm vs-info">
+          <div className="vs-min-max col-3 center-elm">
+            <table className="vs-min-max table thead-dark table-striped table-bordered mt-2 w-auto">
               <tbody>
                 <tr>
                   <td className="bold">Min Depth</td>
-                  {/* <td>{Utils.roundValue(vsProfileInfo["z_min"])}m</td> */}
+                  <td>{Utils.roundValue(vsProfileInfo["z_min"])}m</td>
                 </tr>
                 <tr>
                   <td className="bold">Max Depth</td>
-                  {/* <td>{Utils.roundValue(vsProfileInfo["z_max"])}m</td> */}
+                  <td>{Utils.roundValue(vsProfileInfo["z_max"])}m</td>
                 </tr>
               </tbody>
             </table>
           </div>
-          <div className="spt-removed col-3 center-elm">
-            <table className="spt-removed table thead-dark table-striped table-bordered mt-2 w-auto">
+          <div className="vs-removed col-3 center-elm">
+            <table className="vs-removed table thead-dark table-striped table-bordered mt-2 w-auto">
               <tbody>
                 <tr>
                   <td className="bold">Depth Spread</td>
-                  {/* <td>{Utils.roundValue(vsProfileInfo["z_spread"])}m</td> */}
+                  <td>{Utils.roundValue(vsProfileInfo["z_spread"])}m</td>
                 </tr>
                 <tr className="highlight">
                   <td className="bold">Removed Rows</td>
-                  {/* <td>{vsProfileInfo["removed_rows"].length}</td> */}
+                  <td>{vsProfileInfo["removed_rows"].length}</td>
                 </tr>
               </tbody>
             </table>
