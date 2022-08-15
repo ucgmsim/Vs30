804b1e8f44e8b57d7b053c17faf3768c19b8953e
joelridden <jriddenw@gmail.com>
2022-08-15 14:35:35 +1200

    Removing VsViewer imports

--- VsViewer/setup.py
+++ VsViewer/setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 setup(
     name="VsViewer",
     version="1.0",
-    packages=["vs_calc"],
+    packages=["vs_calc", "vs_api"],
     url="https://github.com/ucgmsim/Vs30",
     description="Vs30 Web Calculator",
     install_requires=["numpy", "pandas", "pyyaml", "flask", "flask_cors", "uwsgi"],
